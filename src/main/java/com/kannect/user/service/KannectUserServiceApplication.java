5aea9c4b2c519c416e3347dc72f92eb914424edb
shreyasshelarfio <shreyas.shelar@fio.sg>
2025-05-11 01:18:14 +0530

    fix

--- src/main/java/com/kannect/user/service/KannectUserServiceApplication.java
+++ src/main/java/com/kannect/user/service/KannectUserServiceApplication.java
@@ -2,9 +2,13 @@ package com.kannect.user.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.kannect.user.service")
+@EntityScan(basePackages = "com.kannect.user.service")
 @EnableFeignClients(basePackages = "com.kannect.user.service.masters.service")
 public class KannectUserServiceApplication {
 
